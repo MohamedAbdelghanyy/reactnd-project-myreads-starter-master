c4c1a70c3f28480a91cefc9607f766720a6c13a0
Mohamed Abdelghany <mohamedabdelghanyy@gmail.com>
2021-11-26 21:16:39 +0200

    Update SearchBooks.js

--- src/comps/SearchBooks.js
+++ src/comps/SearchBooks.js
@@ -2,45 +2,57 @@ import React, { useState } from "react"
 import Book from "./Book"
 import { Link } from "react-router-dom"
 import * as BooksAPI from '../BooksAPI'
+import PropTypes from 'prop-types'
 
-const SearchBooks =(props) => {
-    let [searchKey, setSearchKey] = useState("");
-    let [searchBooksList, setBooksX] = useState([]);
+class SearchBooks extends React.Component {
 
-    const APISearch = () => {
-        BooksAPI.search(searchKey).then((result) => {
+    static propTypes = {
+        allBooks: PropTypes.array.isRequired,
+        changeShelf: PropTypes.func.isRequired
+    };
+    
+    state = {
+        searchKey: '',
+        searchBooksList: [],
+        isErr: false
+    };
+
+    APISearch () {
+        BooksAPI.search(this.state.searchKey).then((result) => {
             if(result && result.length > 0){
                 for(let i = 0 ; i < result.length ; i++){
-                    for(let j = 0 ; j < props.allBooks.length ; j++){
-                        if(result[i].id === props.allBooks[j].id){
-                            let bookI = props.allBooks.findIndex((book) => book.id === result[i].id);
-                            result[i].shelf = props.allBooks[bookI].shelf;
+                    for(let j = 0 ; j < this.props.allBooks.length ; j++){
+                        if(result[i].id === this.props.allBooks[j].id){
+                            result[i].shelf = this.props.allBooks[this.props.allBooks.findIndex((book) => book.id === result[i].id)].shelf;
                         }
                     }
                 }
             }
-            setBooksX(result);
+            this.setState({searchBooksList: result});
         })
     }
     
-    const searchBooksF = (searchKey) => {
-        setSearchKey(searchKey);
-        APISearch();
+    searchBooksF(searchKey){
+        this.setState({searchKey: searchKey});
+        this.APISearch();
     }
 
-    return  (<div className="search-books">
-                    <div className="search-books-bar">
-                        <Link className="close-search" to="/">Close</Link>
-                        <div className="search-books-input-wrapper">
-                            <input type="text" value={searchKey} onChange={(event) => searchBooksF(event.target.value)} placeholder="Search by title or author" />
+    render (){
+        return  (<div className="search-books">
+                        <div className="search-books-bar">
+                            <Link className="close-search" to="/">Close</Link>
+                            <div className="search-books-input-wrapper">
+                                <input type="text" value={this.state.searchKey} onChange={(event) => this.searchBooksF(event.target.value)} placeholder="Search by title or author" />
+                            </div>
                         </div>
-                    </div>
-                    <div className="search-books-results">
-                        <ol className="books-grid">
-                            {searchBooksList && searchBooksList.length > 0 && searchBooksList.map((book) => (<Book key={book.id} onShelfChange={props.onShelfChange} bookData={book} />))} 
-                        </ol>
-                    </div>
-                </div>);      
+                        {this.state.searchKey.length !== 0 && (
+                        <div className="search-books-results">
+                            <ol className="books-grid">
+                                {this.state.searchBooksList && this.state.searchBooksList.length > 0 && this.state.searchBooksList.map((book) => (<Book key={book.id} onShelfChange={this.props.onShelfChange} bookData={book} />))} 
+                            </ol>
+                        </div>)}
+                    </div>);   
+    }   
 }
 
 export default SearchBooks
